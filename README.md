af97c933b4a6adab7d2cd222c3c8a060775b41dc
IIMaksymilianDymitrII <maksymiliandymitr@chasacademy.se>
2025-10-08 12:08:49 +0200

    Small corrections, put user info to a localstorage and making the form work but not done yet

--- README.md
+++ README.md
@@ -1,33 +1,24 @@
 # Bootcamp-Projekt
 
-
-
-
 ## ToDO List
 
 ### Main Objectives (mostly for me)
-
 [✅] responsive design
 [✅] keyboard navigation (works not best but works)
-[] following WCAG 2.2 (letter color is to close to the color of the bg)
-[] calculator for counting the price
-[] creating a json file with info
-[] form js
-[] Write description of project in readme.md (here)'
-
+[✅] following WCAG 2.2
+    [✅]  (letter color is to close to the color of the bg)
+    [✅]  keyboard acceasablity (could be done better)
+[] Form
+    [✅] Calculator for counting the price
+    [✅] Save in localStorage
+    [] Make a popup
+[] Write description of project in readme.md (here)
 
 [] Write a doc
-
-- [] Explain design & acceasblity choice made in the project
-- [] Explain how git was used in this project
-- [] Reflect on the way of working (how you organized yourselves within the group, what challenges you encountered and whether they are lessons you have learned for the next task).
-
+    [] Explain design & acceasblity choice made in the project
+    [] Explain how git was used in this project
+    [] Reflect on the way of working (how you organized yourselves within the group, what challenges you encountered and whether they are lessons you have learned for the next task).
 
 ### Not so important but fun
 [] Tracking user and storing the data
 [] make an intro to the page also called a splash
-
---------------------------------------------------
-form
-everything has to be filled
-then calculate the price (teoriyetical)
--- main.js
+++ main.js
@@ -6,16 +6,87 @@ const subMenuBooking = document.querySelector("#menu-btn-booking");
 const bookingNav = document.querySelector("#main-nav");
 const bookingsNavUl = document.querySelector("#main-nav ul");
 
-if (subMenu && nav && navUl)  {    
-    subMenu.addEventListener("click", () => {
-        nav.classList.toggle("show");
-        navUl.classList.toggle("show");
-    });
+if (subMenu && nav && navUl) {
+  subMenu.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    navUl.classList.toggle("show");
+  });
 }
+
+if (subMenuBooking && bookingNav && bookingsNavUl) {
+  subMenuBooking.addEventListener("click", () => {
+    bookingsNavUl.classList.toggle("show");
+    bookingNav.classList.toggle("show");
+  });
+}
+
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const dateInput = document.querySelector("#date");
+const serivceSelect = document.querySelector("#service");
+const livingAreaInput = document.querySelector("#living-area");
+const messageInput = document.querySelector("#message");
+
+
+form.addEventListener("submit", (e) => {
+  function ShowPopup(data, price) {
+
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
     
-if (subMenuBooking && bookingNav && bookingsNavUl){
-    subMenuBooking.addEventListener("click", () => {
-        bookingsNavUl.classList.toggle("show");
-        bookingNav.classList.toggle("show");
+    const textPopup = document.createElement("p");
+    textPopup.textContent =
+      "Tack för din bokning! Vi återkommer så snart som möjligt.";
+
+    const dataPopup = document.createElement("div");
+    popupData.textContent = data;
+
+    const pricePopup = document.createElement("p");
+    pricePopup.textContent = `Pris för vald tjänst: ${price} Kr`;
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent = "Stäng";
+    closeBtn.classList.add("close-btn");
+    closeBtn.addEventListener("click", () => {
+      document.body.removeChild(popup)
     });
-}
+
+    document.body.appendChild(popup);
+    popup.appendChild(textPopup);
+    popup.appendChild(dataPopup);
+    popup.appendChild(pricePopup);
+  }
+
+  const calculatePrice = (livingArea, service) => {
+    const servicePrice = 0;
+
+    if (service === "hemstädning") {
+      servicePrice = 100;
+    } else if (service === "flyttstädning") {
+      servicePrice = 150;
+    } else if (service === "företagsstädning") {
+      servicePrice = 200;
+    }
+    return livingArea * servicePrice;
+  };
+
+  e.preventDefault();
+
+  const data = {
+    name: nameInput.value,
+    email: emailInput.value,
+    phone: phoneInput.value,
+    date: dateInput.value,
+    livingArea: livingAreaInput.value + "m²",
+    service: serivceSelect.value,
+    message: messageInput.value,
+  };
+
+  let bookingdata = JSON.parse(localStorage.getItem("bookings")) || [];
+  booking.push(data);
+  localStorage.setItem("bookings", JSON.stringify(bookingdata));
+
+  ShowPopup(data, calculatePrice(livingAreaInput.value, serivceSelect.value));
+});
